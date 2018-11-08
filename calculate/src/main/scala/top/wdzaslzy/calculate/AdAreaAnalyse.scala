5972bc35f47e4d5bbbba8c057d87bb05e6e3180a
lizhongyou <lizhongyou@changingedu.com>
2018-11-08 19:05:21 +0800

    spark core 计算区域分布情况

--- calculate/src/main/scala/top/wdzaslzy/calculate/AdAreaAnalyse.scala
+++ calculate/src/main/scala/top/wdzaslzy/calculate/AdAreaAnalyse.scala
@@ -13,7 +13,11 @@ object AdAreaAnalyse {
       .master("local[4]")
       .getOrCreate()
 
-    method1(sparkSession)
+    //spark sql来实现
+//    method1(sparkSession)
+
+    //spark core来实现
+    method2(sparkSession)
 
     sparkSession.close()
   }
@@ -63,6 +67,44 @@ object AdAreaAnalyse {
     resultFrame.show()
   }
 
-  def method2(sparkSession: SparkSession): Unit = {}
+  //CORE形式
+  def method2(sparkSession: SparkSession): Unit = {
+    val lineRdd = sparkSession.sparkContext.textFile("E:\\adsystem\\log\\adClickLog.txt")
+    //1. 取出所有字段
+    val fieldRdd = lineRdd.map(line => {
+      val infos = line.split("\\|")
+      (infos(0), infos(1).toInt, infos(2).toInt, infos(3).toInt, infos(4).toDouble,
+        infos(5), infos(6), infos(7), infos(8), infos(9).toInt)
+    })
+    //2. 将字段转换为有效指标
+    val targetRdd = fieldRdd.map(fields => {
+      var num = 1
+      var effectNum = 0
+      var showNum = 0
+      var clickNum = 0
+      var price = 0.0
+
+      if (fields._10 == 1) {
+        effectNum += 1
+        price += fields._5
+      }
+
+      if (fields._4 == 1) {
+        showNum += 1
+      } else {
+        clickNum += 1
+      }
+
+      //城市, (省份，数量，有效数量，展示量，点击量，价格)
+      (fields._8, (fields._9, num, effectNum, showNum, clickNum, price))
+    })
+
+    val reducedRdd = targetRdd.reduceByKey((a, b) => {
+      (a._1, a._2 + b._2, a._3 + b._3, a._4 + b._4, a._5 + b._5, a._6 + b._6)
+    })
+    val result = reducedRdd.collect()
+
+    println(result.toBuffer)
+  }
 
 }
